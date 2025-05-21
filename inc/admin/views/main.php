5c1571862b6a06e0d7246d29fd259997b5f7ea55
mehdi1413 <mehdifani4@gmail.com>
2025-05-22 02:09:29 +0330

    add main admin view

---
+++ inc/admin/views/main.php
@@ -0,0 +1,4 @@
+<div class="wrap">
+	<h1>main page zoho crm</h1>
+    <p>This page is for configuring the display of various sections of the WordPress to Zoho CRM plugin, which you can perform using CRUD operations.</p>
+</div>
